0f2128cc3d02ee4a170f3ebfb1e958fe055fd362
kavithachunduri <chunduri.kavitha@gmail.com>
2022-08-04 22:00:36 -0400

    plant-api

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const { Router } = require('express')
+const controllers = require('../controllers')
+const router = Router();
+
+router.get('/', (req, res) => res.send('This is the root!'))
+
+router.post('/plants', controllers.createPlant)
+
+router.get('/plants', controllers.getAllPlants)
+
+router.get('/plants/:id', controllers.getPlantById)
+
+router.put('/plants/:id', controllers.updatePlant)
+router.delete('/plants/:id',controllers.deletePlant)
+
+module.exports = router;
